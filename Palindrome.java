e519d03fa40f897f37826b7e115aff38566b0910
dc <danielmcollins1996@gmail.com>
2020-04-09 13:12:09 -0600

    true and false statement

--- Palindrome.java
+++ Palindrome.java
@@ -1,5 +1,22 @@
-class Solution {
+import java.util.Scanner;
+
+class Palindrome {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Check for Palindrome, enter a number: ");
+        int number = scan.nextInt();
+        Palindrome p = new Palindrome();
+
+        if(p.isPalindrome(number)) {
+            System.out.println("It is a palindrome");
+        } else {
+            System.out.println("It is not a palindrome");
+        }
+    }
+
     public boolean isPalindrome(int x) {
+        // int x = 45;
         int temp = x;
         int r = 0;
         int sum = 0;
